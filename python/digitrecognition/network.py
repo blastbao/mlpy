df1a4ff3d4d835029151cc8e5b66256a6dd592c2
Kevin Wan <kevin@liao.com>
2016-09-25 22:31:29 +0800

    initial import of digit recognition

---
+++ python/digitrecognition/network.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class Network(object):
+    def __init__(self, sizes):
+        self.sizes = sizes
+        self.num_layers = len(sizes)
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+
+    @staticmethod
+    def sigmoid(z):
+        return 1.0 / (1.0 + np.exp(-z))
