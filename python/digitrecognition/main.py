38ccb335646da475fda5858ba498736a01c69c6e
kevin <kevin@liao.com>
2016-09-28 16:40:46 +0800

    network2 done

--- python/digitrecognition/main.py
+++ python/digitrecognition/main.py
@@ -1,7 +1,8 @@
 import mnist_loader
-import network
+import network2 as network
 
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = network.Network([784, 30, 10])
-net.stochatic_gradient_descent(training_data, 30, 10, 3.0, test_data)
+net = network.Network([784, 100, 10])
+net.default_weight_initializer()
+net.stochastic_gradient_descent(training_data, 30, 10, 0.5, evaluation_data=test_data, monitor_evaluation_accuracy=True)
